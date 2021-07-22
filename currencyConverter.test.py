40791cbbdb725595538f5755df0cf6fbd9254d50
dgottfried-qa <dgottfried@rogers.com>
2021-07-22 17:40:43 -0400

    Setup Clean up

--- currencyConverter.test.py
+++ currencyConverter.test.py
@@ -1,4 +1,36 @@
 from selenium import webdriver
 driver = webdriver.Firefox(executable_path=r'C:\Users\Daniel G\Downloads\geckodriver.exe')
 
-driver.get("https://www.bloomberg.com/markets/currencies")
+#  Old setup test
+#  driver.get("https://www.bloomberg.com/markets/currencies")
+#  EUR_USD_Change_Value = driver.find_element_by_xpath("//table[@class='data-table']/tbody/tr[1]/td[2]/span").text
+#  EUR_USD_Change_Color = 
+#  driver.close()
+#  print (EUR_USD_Change)
+
+
+class currencyConverter:
+    # setup
+    # Test data type worse is red
+    # Test data type better is green
+    # Test values above 0 are data type better
+    # Test values below 0 are data typer worse
+    # clean up
+
+    def setup():
+        driver.get("https://www.bloomberg.com/markets/currencies")
+        print("open webpage success")
+    
+    def cleanUp():
+        driver.close()
+        print("close webpage success")
+    
+    
+
+cc = currencyConverter
+cc.setup()
+cc.cleanUp()
+    
+    # driver.get("https://www.bloomberg.com/markets/currencies")
+    # if (datatype worse is equal to red):
+    #    return True
