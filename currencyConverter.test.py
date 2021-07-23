564016d25f41fa7efa8c492f6e87e59237e9be48
dgottfried-qa <dgottfried@rogers.com>
2021-07-23 09:02:28 -0400

    Added test

--- currencyConverter.test.py
+++ currencyConverter.test.py
@@ -1,4 +1,6 @@
+import unittest
 from selenium import webdriver
+from selenium.webdriver.support.color import Color
 driver = webdriver.Firefox(executable_path=r'C:\Users\Daniel G\Downloads\geckodriver.exe')
 
 #  Old setup test
@@ -9,28 +11,30 @@ driver = webdriver.Firefox(executable_path=r'C:\Users\Daniel G\Downloads\geckodr
 #  print (EUR_USD_Change)
 
 
-class currencyConverter:
-    # setup
+class CurrencyConverter(unittest.TestCase):
+    def setUp(self):
+        driver.get("https://www.bloomberg.com/markets/currencies")
+        #  print("open webpage success")
+
     # Test data type worse is red
+    def test_worse_should_be_red(self):
+        #  print('worseShouldBeRed executed')
+        
+        worseElements = driver.find_elements_by_xpath("//table[@class='data-table']/tbody/tr/td[@data-type='worse']/span")
+        actual = worseElements[0].value_of_css_property("background-color")
+        expected = Color.from_string('rgb(255, 225, 225)')
+        self.assertEqual(actual, expected.rgb, 'Worse is not red')
+
+        #  print(len(worseElements))
+        #  print(worseElements[0].value_of_css_property("background-color"))
+
     # Test data type better is green
     # Test values above 0 are data type better
     # Test values below 0 are data typer worse
-    # clean up
-
-    def setup():
-        driver.get("https://www.bloomberg.com/markets/currencies")
-        print("open webpage success")
     
-    def cleanUp():
+    def tearDown(self):
         driver.close()
-        print("close webpage success")
+        #  print("close webpage success")
     
-    
-
-cc = currencyConverter
-cc.setup()
-cc.cleanUp()
-    
-    # driver.get("https://www.bloomberg.com/markets/currencies")
-    # if (datatype worse is equal to red):
-    #    return True
+if __name__ == '__main__':
+    unittest.main()
